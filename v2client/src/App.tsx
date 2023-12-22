3d6995d40ab2afe585f64a685ed898547f0447b6
R894 <kamil.khalaileh@gmail.com>
2023-12-22 21:53:46 +0200

    update basic UI skeleton

--- v2client/src/App.tsx
+++ v2client/src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex h-screen">
+      <div className="w-1/12 flex flex-col justify-between bg-primary-content p-4">
+        Users
+        <div className="">
+          User info and config
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="flex flex-col justify-between flex-grow p-4">
+        Chat
+        <input type="text" placeholder="Type here..." className="input input-bordered w-full">
+        </input>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ v2client/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
+
