6867d8c8011e05416a987b86c9d5299376997134
R894 <kamil.khalaileh@gmail.com>
2023-12-29 22:11:14 +0200

    modify GetMessages to include username

--- go-server/internal/rest/handlers/message.go
+++ go-server/internal/rest/handlers/message.go
@@ -1,11 +1,17 @@
 package handlers
 
 import (
+	"go-chatserver/internal/models"
 	"go-chatserver/internal/repository"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
+
+type MessageWithUser struct {
+	Message  models.Message `json:"message"`
+	UserName string         `json:"userName"`
+}
 
 func CreateMessage(c *gin.Context, r *repository.Repository) {
 	var createMessageRequest repository.CreateMessageRequest
@@ -23,12 +29,42 @@ func CreateMessage(c *gin.Context, r *repository.Repository) {
 }
 
 func GetMessages(c *gin.Context, r *repository.Repository) {
+	// TODO: this is inefficient, think of a way to improve this later
 	chatId := c.Param("chatId")
-	messages, err := r.Messages.FindByChatID(c, chatId)
 
+	messages, err := r.Messages.FindByChatID(c, chatId)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(http.StatusOK, messages)
+
+	var messagesWithUser []MessageWithUser
+
+	// using goroutines reduces request time but its still slow
+	resultChan := make(chan MessageWithUser)
+
+	for _, msg := range messages {
+		go func(message *models.Message) {
+			user, err := r.Users.FindByID(c, message.SenderId)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
+
+			messageWithUser := MessageWithUser{
+				Message:  *message,
+				UserName: user.Name,
+			}
+
+			resultChan <- messageWithUser
+		}(msg)
+	}
+
+	for range messages {
+		messageWithUser := <-resultChan
+		messagesWithUser = append(messagesWithUser, messageWithUser)
+	}
+
+	c.JSON(http.StatusOK, messagesWithUser)
+	close(resultChan)
 }
