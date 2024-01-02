80f65e0c537d11ae699f34fa16cee86e034af616
R894 <kamil.khalaileh@gmail.com>
2024-01-02 11:17:21 +0200

    add loading indicator for messages

--- client/src/components/Chatbox/MessageList.tsx
+++ client/src/components/Chatbox/MessageList.tsx
@@ -1,20 +1,24 @@
+import { useContext } from "react";
 import { Message } from "../../types/types";
 import MessageComponent from "../Message";
+import { ChatContext } from "../../context/ChatContext";
 
 const MessageList = ({ messages }: { messages: Message[] }) => {
+  const {isChatLoading} = useContext(ChatContext);
+
   if (!messages) {
     return;
   }
 
   return (
     <div className="flex-grow p-3 overflow-y-auto">
-      {messages.map((message, index) => (
+      {!isChatLoading? messages.map((message, index) => (
         <MessageComponent
           key={index}
           name={message.userName}
           content={message.message.text}
         />
-      ))}
+      )): 'Loading...'}
     </div>
   );
 };
