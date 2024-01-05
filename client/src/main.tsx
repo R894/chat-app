2b9818bcd4a24e1eae0ad08a91637826c5977661
R894 <kamil.khalaileh@gmail.com>
2024-01-05 15:21:50 +0200

    update formatting

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <AuthContextProvider>
         <ChatContextProvider>
-        <App />
+          <App />
         </ChatContextProvider>
       </AuthContextProvider>
     </BrowserRouter>
